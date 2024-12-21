1eaa77d9fd1839d926dfc6ad46d1d3016158e376
Kunnawuth Meebang <folknawat64@gmail.com>
2024-12-21 22:48:49 +0700

    rename folder

---
+++ pylangnoi/base.py
@@ -0,0 +1,86 @@
+import os
+import sys
+from dotenv import load_dotenv
+if __name__ == "__main__":
+    sys.path[0] = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from typing import List
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain_community.utilities import SQLDatabase
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers.openai_tools import PydanticToolsParser
+from pylangnoi.classmodel import State, Table, QueryOutput
+from pylangnoi.baseprompt import table_prompt, query_prompt
+
+load_dotenv()
+class Langnoi:
+
+    def __init__(self, config: dict):
+        self.db_url = config["db_url"] if "db_url" in config.keys() else "mysql+mysqlconnector://root:root@localhost:3306/Mock_CP_Data"
+        self.apikey = config["apikey"]
+        self.table_prompt = config["table_prompt"] if "table_prompt" in config.keys() else table_prompt
+        self.query_prompt = config["query_prompt"] if "table_prompt" in config.keys() else query_prompt
+
+        # เชื่อม model
+        self.llm = ChatGroq(
+            model="llama-3.2-90b-vision-preview",
+            api_key=self.apikey,
+        )
+
+    def chain_table(self, state: State):
+        """Generate SQL query to fetch information."""
+        llm_with_tools = self.llm.bind_tools([Table])
+        output_parser = PydanticToolsParser(tools=[Table])
+
+        # Database ที่ใช้
+        db = SQLDatabase.from_uri(self.db_url)
+
+        dialect = db.dialect
+        top_k = 10
+        input = state["question"]
+
+        prompt_table = ChatPromptTemplate.from_messages(
+            [
+                ("system", self.table_prompt),
+                ("human", "{question}"),
+            ]
+        )
+
+        category_chain = prompt_table | llm_with_tools | output_parser
+
+        def get_tables(categories: List[Table]) -> List[str]:
+            # [Table(name='countries')] -> ['countries']
+            tables = []
+            for category in categories:
+                tables.append(category.name)
+            return tables
+
+        table_chain = category_chain | get_tables
+        table_answer = table_chain.invoke(input)
+
+        table_info = db.get_table_info(table_answer)
+        
+        query_prompt = self.query_prompt.format(
+            input = input,
+            top_k = top_k,
+            dialect = dialect,
+            table_info = table_info,
+            
+        )
+
+        structured_llm = self.llm.with_structured_output(schema=QueryOutput)
+        result = structured_llm.invoke(query_prompt)
+        return table_answer, {"query": result["query"]}
+
+
+if __name__== "__main__":
+
+    cp_mock = {
+        "apikey": os.getenv['CHAT_API_KEY'],
+    }
+
+    system = Langnoi(cp_mock)
+    result_table, sql_query = system.chain_table(
+        {"question": "Show all data of Product table"}
+    )
+    print(result_table, sql_query)
