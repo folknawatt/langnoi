1e1e77dc94a71cb1e6f67fcd09bfc6929e83839d
Kunnawuth Meebang <folknawat64@gmail.com>
2024-12-22 13:48:05 +0700

    remove some code that is unused

--- setup.py
+++ setup.py
@@ -1,24 +1,12 @@
 import setuptools
-import re
-import os
-
-# path = os.path.join(
-#     os.path.dirname(os.path.abspath(__file__)), "pydatacraft", "__init__.py"
-# )
-# with open(path, "r") as f:
-#     version_file_content = f.read()
-# try:
-#     version = re.findall(r"^__version__ = '([^']+)'\r?$", version_file_content, re.M)[0]
-# except IndexError:
-#     raise RuntimeError("Unable to determine version")
 
 setuptools.setup(
-    name="langnoi",
+    name="pylangnoi",
     author="langnoi dev team",
     author_email="devteam@langnoi.com",
     description="Langnoi Python Pacakge",
     license="MIT License",
-    version="0.1.4",
+    version="0.1.5",
     packages=setuptools.find_packages(),
     install_requires=[
         'langchain-community',
