f44d5ffdcf34a12da321a75025db1422a10f9f02
Kunnawuth Meebang <folknawat64@gmail.com>
2024-12-29 16:17:25 +0700

    add Defualt table_prompt

--- pylangnoi/base.py
+++ pylangnoi/base.py
@@ -43,22 +43,23 @@ class Langnoi:
         top_k = 10
         input = state["question"]
 
-        # # Defualt table_prompt #################################################################################
-        # if self.table_prompt == "" :
-        #     try:
-        #         # Fetch table names and construct the default table prompt
-        #         table_names = "\n".join(db.get_usable_table_names())
-        #         self.table_prompt = f"""Return the names of ALL the SQL tables that MIGHT be relevant to the user question.\n
-        #             The tables are:\n
+        # Defualt table_prompt #################################################################################
+        if self.table_prompt == "" :
+            try:
+                # Fetch table names and construct the default table prompt
+                table_names = "\n".join(db.get_usable_table_names())
+                self.table_prompt = f"""Return the names of ALL the SQL tables that MIGHT be relevant to the user question.\n
+                    The tables are:\n
 
-        #             {table_names}
+                    {table_names}
 
-        #             Remember to include ALL POTENTIALLY RELEVANT tables, even if you're not sure that they're needed."""
+                    Remember to include ALL POTENTIALLY RELEVANT tables, even if you're not sure that they're needed."""
 
-        #     except Exception as e:
-        #         # Handle potential errors in fetching table names
-        #         raise RuntimeError("Failed to fetch usable table names.") from e
-        # ########################################################################################################
+            except Exception as e:
+                # Handle potential errors in fetching table names
+                raise RuntimeError("Failed to fetch usable table names.") from e
+        ########################################################################################################
+        
         prompt_table = ChatPromptTemplate.from_messages(
             [
                 ("system", self.table_prompt),
